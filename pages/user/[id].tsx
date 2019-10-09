10789a1f47577b12fdcd39fd075245b161740470
sbogdaniuk <sergei.bogdaniuk@gmail.com>
2019-10-09 20:57:07 +0300

    style(format): apply prettier to pages

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
+import { gql } from 'apollo-boost'
 
 import { withApollo } from '../../lib'
-import { ApolloError } from 'apollo-client'
 
 interface User {
   id: string
@@ -28,20 +27,16 @@ const User: NextPage = () => {
   const router = useRouter()
   const { id } = router.query
   const payload = useQuery<UserProps>(USER, {
-    variables: { id }
+    variables: { id },
   })
-  const { data, error: { graphQLErrors } = {} as ApolloError } = payload
-  const [error] = graphQLErrors ||  []
+  const { data, error: { graphQLErrors = [] } = {} } = payload
+  const [error] = graphQLErrors
   const { user } = data || {}
   return (
     <div>
       <h1>User: {id}</h1>
-      {user && (
-        <div>{user.name}</div>
-      )}
-      {error && (
-        <div>{error.message}</div>
-      )}
+      {user && <div>{user.name}</div>}
+      {error && <div>{error.message}</div>}
     </div>
   )
 }
